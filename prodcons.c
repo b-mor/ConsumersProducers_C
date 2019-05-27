44b8a5d3c8143d4f84fda59008cf8b0cdda68c0b
bbMorris <blm24@uw.edu>
2019-05-26 23:50:28 -0700

    Semi working version. Fixed segfaults, program now runs to completion, successfully works with one producer and one consumer. Aggregate results from multiplications is not yet saved. Debugging messages mostly commented out. Messages to user are more explanatory and easier to follow.

--- prodcons.c
+++ prodcons.c
@@ -1,14 +1,14 @@
 /*
- *  prodcons module
- *  Producer Consumer module
- *
- *  Implements routines for the producer consumer module based on
- *  chapter 30, section 2 of Operating Systems: Three Easy Pieces
- *
- *  University of Washington, Tacoma
- *  TCSS 422 - Operating Systems
- *  Spring 2019
- */
+*  prodcons module
+*  Producer Consumer module
+*
+*  Implements routines for the producer consumer module based on
+*  chapter 30, section 2 of Operating Systems: Three Easy Pieces
+*
+*  University of Washington, Tacoma
+*  TCSS 422 - Operating Systems
+*  Spring 2019
+*/
 
 // Include only libraries for this module
 #include <stdio.h>
@@ -37,18 +37,18 @@ int cons_ptr = 0;      // Index variable for where consumers take new Matrices f
 // bigmatrix = (Matrix**) malloc(sizeof(Matrix) * MAX);
 
 /*
-  Takes a pointer to a Matrix and adds that Matrix to the buffer.
-  Does not handle checking of buffer space or availability, all
-  case coverage must be handled by the calling worker thread.
+Takes a pointer to a Matrix and adds that Matrix to the buffer.
+Does not handle checking of buffer space or availability, all
+case coverage must be handled by the calling worker thread.
 
-  @param value: Pointer to the Matrix to be added to the buffer.
-  @return The index of the next available spot in the buffer.
- */
+@param value: Pointer to the Matrix to be added to the buffer.
+@return The index of the next available spot in the buffer.
+*/
 int put(Matrix * value)
 {
     #if OUTPUT
-    printf("buffer size: %d\n", count);
-    printf("prod_ptr: %d\n", prod_ptr);
+    //printf("buffer size: %d\n", count);
+    //printf("prod_ptr: %d\n", prod_ptr);
     #endif
     bigmatrix[prod_ptr] = value;
     prod_ptr = (prod_ptr + 1) % MAX;
@@ -57,23 +57,23 @@ int put(Matrix * value)
 }
 
 /*
-  Returns a Matrix from the buffer to the caller to be consumed and used
-  in some way. Does not handle checking of buffer space or availability,
-  all coverage must be handled by the calling worker thread.
+Returns a Matrix from the buffer to the caller to be consumed and used
+in some way. Does not handle checking of buffer space or availability,
+all coverage must be handled by the calling worker thread.
 
-  @return The Matrix from the buffer at index determined by cons_ptr.
+@return The Matrix from the buffer at index determined by cons_ptr.
 */
 Matrix * get()
 {
-  #if OUTPUT
-  printf("buffer size: %d\n", count);
-  #endif
+    #if OUTPUT
+    //printf("buffer size: %d\n", count);
+    #endif
     Matrix *temp = bigmatrix[cons_ptr];
     // Increment the index variable, setting it back to 0 if we've reached
     // the end of the buffer's capacity.
     cons_ptr = (cons_ptr + 1) % MAX;
     #if OUTPUT
-    printf("cons ptr: %d\n", cons_ptr);
+    //printf("cons ptr: %d\n", cons_ptr);
     #endif
     count--;
     return temp;
@@ -83,83 +83,100 @@ Matrix * get()
 //TODO put in and test if this works
 void *prod_worker(void *arg)
 {
-  counters_t *myCounters = arg;
-  increment_cnt(myCounters->prod);
-
-  #if OUTPUT
-  printf("Welcome to the producer thread...\n");
-  #endif
-
-  int i;
-  for (i = 0; i < LOOPS; i++) {
+    counters_t *myCounters = arg;
+    increment_cnt(myCounters->prod);
 
     #if OUTPUT
-    printf("producer loop: %d\n", i);
+    printf("Starting producer thread.\n");
     #endif
 
-    pthread_mutex_lock(&mutex);
-    while (count == MAX) {
-      #if OUTPUT
-      printf("in waiting producer\n");
-      #endif
-      pthread_cond_wait(&empty, &mutex);
+    int i;
+    for (i = 0; i < LOOPS; i++) { // Producing Matrix loop.
+
+        #if OUTPUT
+        //printf("producer loop: %d\n", i);
+        #endif
+
+        pthread_mutex_lock(&mutex);
+        while (count == MAX) {
+            #if OUTPUT
+            printf("*** BUFFER FULL, producer is waiting. ***\n\n");
+            #endif
+            pthread_cond_wait(&empty, &mutex);
+        }
+        put(GenMatrixRandom());
+        pthread_cond_signal(&fill);
+        pthread_mutex_unlock(&mutex);
     }
-    put(GenMatrixRandom());
-    pthread_cond_signal(&fill);
-    pthread_mutex_unlock(&mutex);
-  }
 }
 
 // Matrix CONSUMER worker thread
 void *cons_worker(void *arg)
 {
-  #if OUTPUT
-  printf("Welcome to the consumer thread...\n");
-  #endif
-  int i;
-  for (i = 0; i < LOOPS; i++) {
     #if OUTPUT
-    printf("consumer loop: %d\n", i);
+    printf("Starting consumer thread.\n");
     #endif
-    pthread_mutex_lock(&mutex);
-    while (count <= 0) {
-      #if OUTPUT
-      printf("in waiting consumer\n");
-      #endif
-      pthread_cond_wait(&fill, &mutex);
-    }
-    Matrix *m1 = get();
-    Matrix *m2 = get();
-    #if OUTPUT
-    printf("matrix 1\n");
-    DisplayMatrix(m1, stdout);
-    printf("matrix 2\n");
-    DisplayMatrix(m2, stdout);
-    #endif
-    Matrix *m3 = MatrixMultiply(m1, m2); //this is the resultant matrix
-    while (m3 == NULL) { //problem probbably starts here... most likly supposed
-      FreeMatrix(m2);
-      while (count == 0) {
+    int i;
+    for (i = 0; i < LOOPS; i += 2) {
         #if OUTPUT
-        printf("in waiting consumer INNER LOOP\n");
+        // printf("consumer loop: %d\n", i);
         #endif
-        pthread_cond_wait(&fill, &mutex);
-      }
-      m2 = get();
-      m3 = MatrixMultiply(m1,m2);
+        pthread_mutex_lock(&mutex);
+        while (count < 2) {  // Make sure we have two matrices to multiply.
+            #if OUTPUT
+            printf("*** BUFFER EMPTY, consumer is waiting. ***\n\n");
+            #endif
+            pthread_cond_wait(&fill, &mutex);
+        }
+        Matrix *m1 = get();
+        Matrix *m2 = get();
+
+        /**
+        #if OUTPUT
+        printf("matrix 1\n");
+        DisplayMatrix(m1, stdout);
+        printf("matrix 2\n");
+        DisplayMatrix(m2, stdout);
+        #endif
+        */
+
+        Matrix *m3 = MatrixMultiply(m1, m2); //this is the resultant matrix
+/**         while (m3 == NULL) { // Check if multiplication was successful.
+            while (count <= 0) {    // Check buffer for being empty
+                #if OUTPUT
+                printf("in waiting consumer INNER LOOP\n");
+                #endif
+                pthread_cond_wait(&fill, &mutex);
+            }
+
+            if (count > 1) {
+                printf("Inside new check\n");
+                printf("Buffer size: %d\n", count);
+                FreeMatrix(m2);
+                m2 = get();
+                m3 = MatrixMultiply(m1,m2);
+            }
+        }
+
+                                                         */
+        if (m3 != NULL) {  // If successfully multiplied, show results.
+            DisplayMatrix(m1,stdout);
+            printf("    X\n");
+            DisplayMatrix(m2,stdout);
+            printf("    =\n");
+            DisplayMatrix(m3,stdout);
+            printf("\n");
+            FreeMatrix(m1);
+            FreeMatrix(m2);
+            FreeMatrix(m3);
+
+        } else {    // Handle freeing matrices if multiply doesn't work.
+            printf("Matrices were incompatible.\n\n");
+            FreeMatrix(m1);
+            FreeMatrix(m2);
+        }
+
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutex);
     }
-    DisplayMatrix(m1,stdout);
-    printf("    X\n");
-    DisplayMatrix(m2,stdout);
-    printf("    =\n");
-    DisplayMatrix(m3,stdout);
-    printf("\n");
-    // FreeMatrix(m1);
-    // FreeMatrix(m2);
-    // FreeMatrix(m3);
-    pthread_cond_signal(&empty);
-    pthread_mutex_unlock(&mutex);
-
-
-  }
 }
